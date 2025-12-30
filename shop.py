206aa52ec182a19359b70b4301e0b739c610ce2c
Jeevan <jeevanbs2004@gmail.com>
2025-12-30 15:26:31 +0530

    shop classwork1.py

---
+++ shop.py
@@ -0,0 +1,11 @@
+apple_juice=15.5
+orange_juice=20
+grape_juice=10.25
+total=apple_juice+orange_juice+grape_juice
+print(total)
+print("total volume to an integer:",int(total))
+print("total volume to a string:",str(total))
+
+import random
+
+print(random.randrange(5, 10)+total)
